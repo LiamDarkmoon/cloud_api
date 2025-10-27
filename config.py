9b1d708ad1fc59fe0e5fbb59440f9edc2bb50313
Liam Darkmoon <liamdarkmoon@gmail.com>
2025-10-27 20:33:14 -0300

    Refactor settings class to use SettingsConfigDict for environment configuration

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
 
 
 class settings(BaseSettings):
@@ -10,8 +11,11 @@ class settings(BaseSettings):
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
 
-    class Config:
-        env_file = ".env"
+    model_config = (
+        SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+        if os.getenv("VERCEL") is None
+        else SettingsConfigDict()
+    )
 
 
 config = settings()
